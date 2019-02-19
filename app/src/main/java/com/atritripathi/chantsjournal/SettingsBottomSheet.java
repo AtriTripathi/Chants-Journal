a9971f779874e5c2dd0212e4cbba89c5225038a6
Atri Tripathi <tripathi.atri@gmail.com>
2019-02-19 23:55:01 +0530

    Implement app share feature

--- app/src/main/java/com/atritripathi/chantsjournal/SettingsBottomSheet.java
+++ app/src/main/java/com/atritripathi/chantsjournal/SettingsBottomSheet.java
@@ -1,10 +1,11 @@
 package com.atritripathi.chantsjournal;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.constraint.ConstraintLayout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 import android.widget.Toast;
 
 public class SettingsBottomSheet extends RoundedBottomSheetDialogFragment {
@@ -18,20 +19,26 @@ public class SettingsBottomSheet extends RoundedBottomSheetDialogFragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
         View BottomSheetView = inflater.inflate(R.layout.settings_bottom_sheet, container, false);
 
-        TextView shareButton = BottomSheetView.findViewById(R.id.tv_share);
-        TextView donateButton = BottomSheetView.findViewById(R.id.tv_donate);
-        TextView feedbackButton = BottomSheetView.findViewById(R.id.tv_feedback);
-        TextView aboutButton = BottomSheetView.findViewById(R.id.tv_about);
-        TextView exitButton = BottomSheetView.findViewById(R.id.tv_exit);
+        ConstraintLayout shareButton = BottomSheetView.findViewById(R.id.button_share);
+        ConstraintLayout donateButton = BottomSheetView.findViewById(R.id.button_donate);
+        ConstraintLayout feedbackButton = BottomSheetView.findViewById(R.id.button_feedback);
+        ConstraintLayout aboutButton = BottomSheetView.findViewById(R.id.button_about);
+        ConstraintLayout exitButton = BottomSheetView.findViewById(R.id.button_exit);
 
         shareButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getContext(), "Share clicked", Toast.LENGTH_SHORT).show();
+                Intent sharingIntent = new Intent(android.content.Intent.ACTION_SEND);
+                sharingIntent.setType("text/plain");
+                String shareBody = "Install Chants Journal \nSearch on google";
+                String shareSub = "Install Chants Journal";
+                sharingIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, shareSub);
+                sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT, shareBody);
+                startActivity(Intent.createChooser(sharingIntent, "Share Chants Journal via"));
             }
         });
 
