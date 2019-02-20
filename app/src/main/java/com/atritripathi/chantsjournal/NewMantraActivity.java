6607814b03a6729410c6ad006d7b2f10403b5ea3
Atri Tripathi <tripathi.atri@gmail.com>
2019-02-20 21:56:28 +0530

    Implement individual setError with vibration in NewMantraActivity

--- app/src/main/java/com/atritripathi/chantsjournal/NewMantraActivity.java
+++ app/src/main/java/com/atritripathi/chantsjournal/NewMantraActivity.java
@@ -1,14 +1,23 @@
 package com.atritripathi.chantsjournal;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Vibrator;
 import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.EditText;
 
 public class NewMantraActivity extends AppCompatActivity {
+
+    private EditText mantraEditText;
+    private TextInputEditText malasEditText;
+    private Button saveButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,26 +26,50 @@ public class NewMantraActivity extends AppCompatActivity {
 
         overridePendingTransition(R.anim.fade_in_layout,R.anim.fade_out_layout);
 
-        final EditText mantraEditText = findViewById(R.id.et_new_mantra);
-        final TextInputEditText malasEditText = findViewById(R.id.et_total_malas);
-        final Button saveButton = findViewById(R.id.done_chanting_button);
+        mantraEditText = findViewById(R.id.et_new_mantra);
+        malasEditText = findViewById(R.id.et_total_malas);
+        saveButton = findViewById(R.id.done_chanting_button);
 
         saveButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (mantraEditText.getText().toString().trim().equals("")
-                        || malasEditText.getEditableText().toString().trim().equals("")) {
-                    mantraEditText.setError("Mantra Name Required");
-                    malasEditText.setError("Number of Malas Required");
-                } else {
-                    Intent intent = new Intent(NewMantraActivity.this, MainActivity.class);
-                    intent.putExtra("mantra_name", mantraEditText.getText().toString().trim());
-                    intent.putExtra("total_malas",Integer.parseInt(malasEditText.getEditableText().toString().trim()));
-                    setResult(RESULT_OK, intent);
-                    finish();
-                }
+                addMantra();
             }
         });
+    }
+
+
+    private void addMantra() {
+        final String mantra = mantraEditText.getText().toString();
+        final String malas = malasEditText.getEditableText().toString();
+
+        Animation shake = AnimationUtils.loadAnimation(NewMantraActivity.this, R.anim.shake);
+        Vibrator vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+
+        // Mantra is empty
+        if (TextUtils.isEmpty(mantra)) {
+            mantraEditText.startAnimation(shake);
+            mantraEditText.setError("Required");
+            if (vibrator != null)
+                vibrator.vibrate(30);
+            return;
+        }
+
+        // Malas is empty
+        if (TextUtils.isEmpty(malas)) {
+            malasEditText.startAnimation(shake);
+            malasEditText.setError("Required");
+            if (vibrator != null)
+                vibrator.vibrate(30);
+            return;
+        }
+
+        // Save the mantra
+        Intent intent = new Intent(NewMantraActivity.this, MainActivity.class);
+        intent.putExtra("mantra_name", mantraEditText.getText().toString().trim());
+        intent.putExtra("total_malas",Integer.parseInt(malasEditText.getEditableText().toString().trim()));
+        setResult(RESULT_OK, intent);
+        finish();
     }
 }
 
